492a62bb11ba7e92a08cc78cef5fa1e8a38f8b0a
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-04-29 19:41:04 -0300

    posts created by .map

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,26 @@
 import Post from "./Post";
 
 export default function Feed(){
+
+    const posts = [{
+        topLogo:"assets/img/meowed.svg",
+        topUser:"meowed",
+        feedImg:"assets/img/gato-telefone.svg",
+        likeLogo:"assets/img/respondeai.svg",
+        likeUser:"respondeai",
+        likes:"101.523"
+    },{
+        topLogo:"assets/img/barked.svg",
+        topUser:"barked",
+        feedImg:"assets/img/dog.svg",
+        likeLogo:"assets/img/adorable_animals.svg",
+        likeUser:"adorable_animals",
+        likes:"99.159"
+    }]
+
     return(
         <div class="posts">
-            <Post topLogo="assets/img/meowed.svg" topUser="meowed" feedImg="assets/img/gato-telefone.svg" likeLogo="assets/img/respondeai.svg" likeUser="respondeai" likes="101.523" />
-            <Post topLogo="assets/img/barked.svg" topUser="barked" feedImg="assets/img/dog.svg" likeLogo="assets/img/adorable_animals.svg" likeUser="adorable_animals" likes="99.159" />
+            {posts.map( element => <Post topLogo={element.topLogo} topUser={element.topUser} feedImg={element.feedImg} likeLogo={element.likeLogo} likeUser={element.likeUser} likes={element.likes} />)}
         </div>
     );
 }
