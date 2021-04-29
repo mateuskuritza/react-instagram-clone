7262ba5c912b9df6aeda347da7fef5f6db369d34
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-04-29 19:29:43 -0300

    create feed and post components

--- src/Feed.js
+++ src/Feed.js
@@ -1,77 +1,10 @@
+import Post from "./Post";
+
 export default function Feed(){
     return(
         <div class="posts">
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="assets/img/meowed.svg" />
-                        meowed
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                <img src="assets/img/gato-telefone.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="assets/img/respondeai.svg" />
-                        <div class="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                    <img src="assets/img/barked.svg" />
-                    barked
-                    </div>
-                    <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="assets/img/dog.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                    <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    </div>
-
-                    <div class="curtidas">
-                    <img src="assets/img/adorable_animals.svg" />
-                    <div class="texto">
-                        Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                    </div>
-                    </div>
-                </div>
-            </div>
+            <Post topLogo="assets/img/meowed.svg" topUser="meowed" feedImg="assets/img/gato-telefone.svg" likeLogo="assets/img/respondeai.svg" likeUser="respondeai" likes="101.523" />
+            <Post topLogo="assets/img/barked.svg" topUser="barked" feedImg="assets/img/dog.svg" likeLogo="assets/img/adorable_animals.svg" likeUser="adorable_animals" likes="99.159" />
         </div>
     );
 }
